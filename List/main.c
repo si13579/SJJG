7d2ddefa0fa1c03530a19cc097114c005cfbcfaa
李思 <lisi>
2021-06-22 01:48:17 +0800

    062201

--- List/main.c
+++ List/main.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "list.h"
+void printElem(ElemType e){
+    printf("%d\n",e);
+}
 int main(void)
 {
     Sqlist L;
     ElemType e;
     int i;
+    printf("初始化线性表L\n");
     InitList_Sq(&L);
-    InitList_Sq(&L);
+    if (ListEmptpy_Sq(L)) printf("L为空表\n");
+    for (i=1;i<=8;i++){
+        printf("在线性表第%d个位置插入%d\n",i,i*2);
+        ListInsert_Sq(&L,i,i*2);
+    }
+    ListTraverse_Sq(L,printElem);
     return 0;
 }  
